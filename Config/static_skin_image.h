3ba3be4b52826ea7055cce176a02757af7beba73
Alain Royer <Alain.Royer@mttservicecenter.com>
2026-03-27 15:36:56 -0400

    wip

---
+++ Config/static_skin_image.h
@@ -0,0 +1,50 @@
+//-------------------------------------------------------------------------------------------------
+//
+//  File :  Static_skin_image.h
+//
+//-------------------------------------------------------------------------------------------------
+//
+// Copyright(c) 2020 Alain Royer.
+// Email: aroyer.qc@gmail.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+// AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//-------------------------------------------------------------------------------------------------
+//
+//  Notes: X-macro Listing all the image resident in flash
+//
+//-------------------------------------------------------------------------------------------------
+
+#pragma once
+
+//-------------------------------------------------------------------------------------------------
+
+//                  IMAGE_ID        Struct Info Image
+#define STATIC_SKIN_DEF(X_STATIC_SKIN)  \
+    X_STATIC_SKIN(  BOOT_SCREEN,        SII_BootScreen          )\
+    X_STATIC_SKIN(  PROGRESS_CURSOR,    SII_ProgressCursor      )\
+    X_STATIC_SKIN(  PROGRESS_BAR,       SII_ProgressBar         )\
+    X_STATIC_SKIN(  LED_GREEN_OFF,      SII_LedGreenOFF_16x16   )\
+    X_STATIC_SKIN(  LED_GREEN_ON,       SII_Ledgreen_ON_16x16   )\
+    X_STATIC_SKIN(  LED_RED_OFF,        SII_LedRred_OFF_16x16   )\
+    X_STATIC_SKIN(  LED_RED_ON,         SII_LedRed_ON_16x16     )\
+    X_STATIC_SKIN(  LED_BLUE_OFF,       SII_LedBlue_OFF_16x16   )\
+    X_STATIC_SKIN(  LED_BLUE_ON,        SII_LedBlue_ON_16x16    )\
+
+//-------------------------------------------------------------------------------------------------
+
+
+
